d7d5f2325f7c24330f26d83956200045b4635e0a
Benjamin Teubel <29049232+BenschiBox@users.noreply.github.com>
2025-02-11 14:04:41 +0100

    Add files via upload

--- Set-DefCalPermissions.ps1
+++ Set-DefCalPermissions.ps1
@@ -1,11 +1,13 @@
-﻿# Orginal Script written by ALI TAJRAN
-# https://www.alitajran.com/set-default-calendar-permissions-for-all-users-powershell/
-# 
-# Adapted by BenschiBox 07-02-2022
-# https://github.com/BenschiBox/M365-Scripts
-# 
-# This Script utilizes the Exchange Online PowerShell V2 commands (Get-EXO...)
-# Append -whatif to the Set-MailboxFolderPermission command for testing
+﻿<#
+    Orginal Script written by ALI TAJRAN
+    https://www.alitajran.com/set-default-calendar-permissions-for-all-users-powershell/
+
+    Adapted by BenschiBox 11-02-2025
+    https://github.com/BenschiBox/M365-Scripts
+
+    This Script utilizes the Exchange Online PowerShell V3 commands (Get-EXO...)
+    Append -whatif to the Set-MailboxFolderPermission command for testing
+#>
 
 # Start transcript
 Start-Transcript -Path C:\temp\Set-DefCalPermissions.log -Append
@@ -17,25 +19,26 @@ Start-Transcript -Path C:\temp\Set-DefCalPermissions.log -Append
 $Users = Get-EXOMailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox
 
 # Users exception
-$Exception = @("CEO Mailbox", "CTO Mailbox")
+#$Exception = @("CEO Mailbox", "CTO Mailbox")
+$Exception = @( "admin@contoso.com", 
+                "CEO@contoso.com")
 
 # Permissions
 # full list: https://docs.microsoft.com/en-us/powershell/module/exchange/set-mailboxfolderpermission?view=exchange-ps#parameters
 $Permission = "Reviewer"
 
 # Calendar name languages
-$FolderCalendars = @("Agenda", "Calendar", "Calendrier", "Kalender", "日历")
+$FolderCalendars = @("Agenda", "Calendar", "Calendrier", "Kalender", "æ—¥åŽ†")
 
 # Loop through each user
 foreach ($User in $Users) {
 
     # Get calendar in every user mailbox
-    $Calendars = (Get-EXOMailboxFolderStatistics $User.Identity -FolderScope Calendar)
+    $Calendars = (Get-EXOMailboxFolderStatistics $User.UserPrincipalName -FolderScope Calendar)
 
     # Leave permissions if user is exception
     if ($Exception -Contains ($User.UserPrincipalName)) {
-        $ExceptionUPN = $User.UserPrincipalName
-        Write-Host "$ExceptionUPN is an exception, don't touch permissions" -ForegroundColor Red
+        Write-Host "$User is an exception, don't touch permissions" -ForegroundColor Red
     }
     else {
 
@@ -45,20 +48,18 @@ foreach ($User in $Users) {
 
             # Check if calendar exist
             if ($FolderCalendars -Contains $CalendarName) {
-                $Cal = $User.Identity.ToString() + ":\$CalendarName"
+                $Cal = "$($User.UserPrincipalName):\$CalendarName"
                 $CurrentMailFolderPermission = Get-EXOMailboxFolderPermission -Identity $Cal -User Default
                 
-                # Set calendar permission / Remove -WhatIf parameter after testing
-                Set-MailboxFolderPermission -Identity $Cal -User Default -AccessRights $Permission -WarningAction:SilentlyContinue #-WhatIf
-                
-                # Write output
-                if ($CurrentMailFolderPermission.AccessRights -eq "$Permission") {
-                    Write-Host $User.Identity already has the permission $CurrentMailFolderPermission.AccessRights -ForegroundColor Yellow
+                # Update calendar permissions if necessary
+                if ($CurrentMailFolderPermission.AccessRights -ne "$Permission") {
+                    # Set calendar permission / Remove -WhatIf parameter after testing
+                    Set-MailboxFolderPermission -Identity $Cal -User Default -AccessRights $Permission -WarningAction:SilentlyContinue #-WhatIf
+                    Write-Host $User.DisplayName added permissions $Permission -ForegroundColor Green
                 }
                 else {
-                    Write-Host $User.Identity added permissions $Permission -ForegroundColor Green
-                }
-            }
+                    Write-Host $User.DisplayName already has the permission $Permission -ForegroundColor Yellow
+                }            }
         }
     }
 }
